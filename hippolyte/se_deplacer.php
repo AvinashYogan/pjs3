fd25601498194358610f82fb8f0f23233e7d330a
hippohelson <33905630+hippohelson@users.noreply.github.com>
2017-11-29 18:05:13 +0100

    Add files via upload
    upload partie hippolyte

---
+++ hippolyte/se_deplacer.php
@@ -0,0 +1,54 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Région paca</title>
+		<link rel="stylesheet" href="css/foundation.css">
+		<link rel="stylesheet" href="css/app.css"> 
+		<link rel="stylesheet" href="css/style.css">
+		<style>
+			div.container {
+				text-align: center;
+				margin-left: auto;
+				margin-right: auto;
+			}
+			
+			div.container img {
+				width: 100%;
+			}
+		</style>
+	</head>
+	<body>
+		<?php include('./header.html');?>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-2">
+				</div>
+				<div class="col-lg-6 col-md-8 col-sm-12">
+					<a href="./dans_la_region.php">
+						<img src="./images/cartebus.jpg">
+					</a>
+				</div>
+			</div>
+		</div>
+		<br>
+		<div class="container">
+			<div class="row">
+				<div class="col-lg-3 col-md-2">
+				</div>
+				<div class="col-lg-6 col-md-8 col-sm-12" >
+					<a href="./y_aller.php">
+						<img src="./images/logopaca.jpg">
+					</a>
+				</div>
+			</div>
+		</div>
+		<script src="js/vendor/jquery.js"></script>
+		<script src="js/vendor/what-input.js"></script>
+		<script src="js/vendor/foundation.js"></script>
+		<script src="js/app.js"></script>
+	</body>
+</html>
