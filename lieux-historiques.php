fd996fd715d71d26e81eb1c7bd179692bce367f7
ThibaultMassu <33905717+ThibaultMassu@users.noreply.github.com>
2017-11-29 18:01:00 +0100

    Add files via upload

---
+++ lieux-historiques.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="css/bootstrap/bootstrap.min.css">
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<?php include('menu.html'); ?>
+		<div class="container">
+			<div class="titre">
+				<h1>Lieux Historiques</h1>
+			</div>
+			<div class="row">
+				<div class="images3">
+					<a href="http://www.theatre-antique.com/" target="_blank"><img src="img/lieux-historiques/Théâtre_Antique_d_Orange.jpg"></a>
+					<a href="https://www.saint-pauldevence.com/" target="_blank"><img src="img/lieux-historiques/Saint-Paul_De_Vence.jpg"></a>
+					<a href="http://www.villa-ephrussi.com/" target="_blank"><img src="img/lieux-historiques/Villa_Ephrussi.jpg"></a>
+
+				</div>
+			</div>
+			<div class="row">
+				<div class="images3">
+					<a href="https://fr.wikipedia.org/wiki/Vieux-Nice" target="_blank"><img src="img/lieux-historiques/Vieux_Nice.jpg"></a>
+					<a href="http://www.avignon-pont.com/fr" target="_blank"><img src="img/lieux-historiques/Pont_Benezet.jpg"></a>
+					<a href="https://fr.wikipedia.org/wiki/Place_Mass%C3%A9na" target="_blank"><img src="img/lieux-historiques/Place_Massena.jpg"></a>
+				</div>
+			</div>
+			<div class="row">
+				<div class="images3">
+					<a href="http://www.palais-des-papes.com/fr" target="_blank"><img src="img/lieux-historiques/Palais_Des_Papes.jpg"></a>
+					<a href="http://www.marseilletourisme.fr/les-eglises/cathedrale-major/" target="_blank"><img src="img/lieux-historiques/Cathédrale_De_La_Major.jpg"></a>
+					<a href="http://www.notredamedelagarde.com/" target="_blank"><img src="img/lieux-historiques/Notre_Dame_De_La_Garde.jpg"></a>
+				</div>
+			</div>
+		</div>
+		<?php include('footer.html'); ?>
+	</body>
+</html>
