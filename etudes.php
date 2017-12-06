0461477848fc106f2cbe3bad94d81345fee47171
Pierre Couderc <33221719+Nebuchadnezzard@users.noreply.github.com>
2017-12-06 17:50:46 +0100

    Add files via upload

---
+++ etudes.php
@@ -0,0 +1,61 @@
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>Etudes</title>
+		<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/stylePierre.css">
+	</head>
+	<body>
+		<?php include('menu.html'); ?>
+		<div class="container">
+			<h2>Etudes</h2>
+			<p>La région PACA dispose d’un certain nombre d’institutions d’études supérieures,
+			dont la faculté de droit de Marseille qui dispose d’une excellente réputation.
+			Il existe 4 grands centres universitaires, plusieurs écoles d’ingénieurs, six 
+			technopoles.
+			La région est très orientée vers la recherche avec 17000 chercheurs et enseignant 
+			chercheurs et un centre du Commissariat à l’Energie Atomique, 2 Centres Nationaux 
+			de la Recherche Scientifique (CNRS), un Institut Français des Sciences Technologiques
+			des Transports (IFSTTAR), un Institut National de la Recherche Agricole (INRA), etc...
+			</p>
+			
+			<img class="etudes" src="img/imgPierre/universite.png">
+			
+			<h3>L'Académie d'Aix-Marseille</h3>
+			<p>Elle comprend quatre départements (Bouches du Rhône, Vaucluse, Hautes Alpes et Alpes 
+			de Haute Provence), 16 bassins de formation et compte 2,9 millions d'habitants. 
+			L’académie d’Aix-Marseille comptait 102 388 étudiants en 2011-2012, principalement regroupés
+			à Aix-en-Provence et Marseille. Près de deux tiers des étudiants ont effectué leur cursus 
+			en université, dont : 18,4 % en Lettres et 12,7 % en Sciences et S.T.A.P.S. ; 15,2 % des 
+			étudiants ont suivi un cycle en deux ans en I.U.T.ou en S.T.S.
+			Entre 2000 et 2012, le nombre d’étudiants a augmenté de 49,7 % en filière Santé, de 20,2 %
+			en Droit et de 15,4 % en Économie et les C.P.G.E. de 17,1 %. Les effectifs des formations
+			sciences et lettres, en revanche, continuent à baisser (-23% et -21%). L’académie d’Aix-Marseille 
+			comprenait, en 2011-2012, 102 388 étudiants principalement regroupés à Aix-en- Provence et
+			Marseille. Près de deux tiers des étudiants ont effectué leur cursus en université, dont 
+			18,4 % en lettres et 12,7 % en sciences et S.T.A.P.S,. 15,2% des étudiants ont suivi un 
+			cycle en deux ans en I.U.T. ou en S.T.S.
+			</p>
+			
+			<img class="etudes" src="img/imgPierre/polytech.png">
+			
+			<h3>L'Académie de Nice</h3>
+			<p>Elle comprend deux départements (Alpes Maritimes et Var), 3 bassins de formation et 
+			compte 2 millions d'habitants. L'académie de Nice comptait en 49 640 étudiants en 2011-2012 
+			, principalement regroupés à Nice, Sophia Antipolis, Toulon La Seyne. 34 890 sont inscrits
+			à l'Université, 2150 en classes préparatoire aux grandes écoles, 12 600 dans les formations 
+			de techniciens supérieurs et I.U.T..La répartition par disciplines des étudiants inscrits
+			donne le résultats suivant : 55% en Lettres et Sciences Economiques; 19,5% en Sciences
+			(soit - 4,8% sur un an), 10,6% en Médecine et Odontologie, 14,6% en IUT et S.T.A.P.S. 
+			Seules les filières Médecine et S.T.A.P.S. sont en progression, Lettres et Odontologie 
+			restent stables et toutes les autres sont en baisse.
+			</p>
+			
+			<img class="etudes" src="img/imgPierre/antipolis.jpg">
+			
+		</div>
+	</body>
+	<?php include('footer.html'); ?>
+
+	
+</html>
