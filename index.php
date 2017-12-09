a8b074f35ef89402ac75fec25c290ae03c651e0c
avinash <avinash.yogan@gmail.com>
2017-12-09 12:30:42 +0100

    Init Avinash

--- index.php
+++ index.php
@@ -6,25 +6,53 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Région paca</title>
 	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="css/bootstrap/bootstrsap.css">
 	<link rel="stylesheet" href="css/app.css"> 
-	<link rel="stylesheet" href="css/style.css"
+	<link rel="stylesheet" href="css/foundation/style.css"
 </head>
 <body>
 
 
-<?php include('menu.html');?>
+  <?php include('menu.html');?>
 
-<div id="Presentation" class="container-fluid">
+  <div id="Presentation" class="container-fluid">
 
-  <video loop muted autoplay  style="width: 100%;" class="fullscreen-bg__video">
-        <source src="img/paca.mp4" type="video/mp4">
-</video>
+    <video loop muted autoplay  style="width: 100%;" class="fullscreen-bg__video">
+      <source src="img/paca.mp4" type="video/mp4">
+      </video>
 
-<div class="title">
-<p>  Region</p>
-	<p> Provence-Alpes-Côtes d'Azure</p>
-</div>
+      <div class="title">
+        <p>  Region</p>
+        <p> Provence-Alpes-Côtes d'Azure</p>
+      </div>
 
+    </div>
+
+    <div class="row">
+      <a href="">
+        <div class="img1">
+         
+          <h5 class="title2">Culture</h5>
+        </div>
+      </a>
+      <a href="">
+       <div class="img2">
+         
+        <h5 class="title2">Géographie</h5>
+      </div>
+    </a>
+    <a href="">
+     <div class="img3">
+       
+      <h5 class="title2">Tourisme</h5>
+    </div>
+  </a>
+  <a href="">
+   <div class="img4">
+     
+    <h5 class="title2">Se Déplacer</h5>
+  </div>
+</a>
 </div>
 
 
@@ -35,3 +63,34 @@
 <script src="js/app.js"></script>
 </body>
 </html>
+
+<script type="text/javascript">
+
+
+
+  //     $('.img1').mouseEnter(function() {
+
+
+  //       $(".title2",this).css("opacity", "1");
+  //       $(".title2",this).css("transition", "0.5");
+  //     });
+
+
+  //     $('.img1').mouseLeave(function() {
+
+  // $(".title2",this).css("transition", "0.5");
+  //       $(".title2",this).css("opacity", "0");
+  
+  //     });
+  
+
+
+
+  // $(".img1").hover(function(){
+
+
+
+
+  //   });
+
+</script>
