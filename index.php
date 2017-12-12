2100cf133040f574fc1fa98dc50ff1151a5dc6c2
ThibaultMassu <33905717+ThibaultMassu@users.noreply.github.com>
2017-12-12 09:19:43 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Région paca</title>
+	<link rel="stylesheet" href="css/foundation.css">
+	<link rel="stylesheet" href="css/bootstrap/bootstrsap.css">
+	<link rel="stylesheet" href="css/app.css"> 
+	<link rel="stylesheet" href="css/foundation/style.css"
+</head>
+<body>
+
+
+  <?php include('menu.html');?>
+
+  <div id="Presentation" class="container-fluid">
+
+    <video loop muted autoplay  style="width: 100%;" class="fullscreen-bg__video">
+      <source src="img/paca.mp4" type="video/mp4">
+      </video>
+
+      <div class="title">
+        <p>  Region</p>
+        <p> Provence-Alpes-Côtes d'Azure</p>
+      </div>
+
+    </div>
+
+    <div class="row">
+      <a href="">
+        <div class="img1 grid">
+         
+          <h5 class="title2">Culture</h5>
+        </div>
+      </a>
+      <a href="">
+       <div class="img2 grid">
+         
+        <h5 class="title2">Géographie</h5>
+      </div>
+    </a>
+    <a href="">
+     <div class="img3 grid">
+       
+      <h5 class="title2">Tourisme</h5>
+    </div>
+  </a>
+  <a href="">
+   <div class="img4 grid">
+     
+    <h5 class="title2">Se Déplacer</h5>
+  </div>
+</a>
+</div>
+
+
+<?php include('footer.html');?>
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/what-input.js"></script>
+<script src="js/vendor/foundation.js"></script>
+<script src="js/app.js"></script>
+</body>
+</html>
+
+<script type="text/javascript">
+
+
+
+</script>
