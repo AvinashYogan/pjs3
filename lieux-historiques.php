4a7be56b56adf1a27edb1dba40fcd035e2622c3a
ThibaultMassu <33905717+ThibaultMassu@users.noreply.github.com>
2017-12-12 09:07:07 +0100

    Update lieux-historiques.php

--- lieux-historiques.php
+++ lieux-historiques.php
@@ -12,25 +12,117 @@
 				<h1 style="text-align: center;">Lieux Historiques</h1>
 			</div>
 			<div class="row">
-				<div class="images3">
-					<a href="http://www.theatre-antique.com/" target="_blank"><img src="img/lieux-historiques/Théâtre_Antique_d_Orange.jpg"></a>
-					<a href="https://www.saint-pauldevence.com/" target="_blank"><img src="img/lieux-historiques/Saint-Paul_De_Vence.jpg"></a>
-					<a href="http://www.villa-ephrussi.com/" target="_blank"><img src="img/lieux-historiques/Villa_Ephrussi.jpg"></a>
-
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Théâtre_Antique_d_Orange.jpg" class="img-fluid">
+						<a href="http://www.theatre-antique.com/" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Théâtre antique d'Orange</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Saint-Paul_De_Vence.jpg" class="img-fluid">
+						<a href="https://www.saint-pauldevence.com/" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Saint-Paul De Vence</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Villa_Ephrussi.jpg" class="img-fluid">
+						<a href="http://www.villa-ephrussi.com/" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Villa Ephrussi</p>
+								</div>
+							</div>
+						</a>
+					</div>
 				</div>
 			</div>
 			<div class="row">
-				<div class="images3">
-					<a href="https://fr.wikipedia.org/wiki/Vieux-Nice" target="_blank"><img src="img/lieux-historiques/Vieux_Nice.jpg"></a>
-					<a href="http://www.avignon-pont.com/fr" target="_blank"><img src="img/lieux-historiques/Pont_Benezet.jpg"></a>
-					<a href="https://fr.wikipedia.org/wiki/Place_Mass%C3%A9na" target="_blank"><img src="img/lieux-historiques/Place_Massena.jpg"></a>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Vieux_Nice.jpg" class="img-fluid">
+						<a href="https://fr.wikipedia.org/wiki/Vieux-Nice" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Vieux Nice</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Pont_Benezet.jpg" class="img-fluid">
+						<a href="http://www.avignon-pont.com/fr" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Pont Bezenet</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Place_Massena.jpg" class="img-fluid">
+						<a href="https://fr.wikipedia.org/wiki/Place_Mass%C3%A9na" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Place Massena</p>
+								</div>
+							</div>
+						</a>
+					</div>
 				</div>
 			</div>
 			<div class="row">
-				<div class="images3">
-					<a href="http://www.palais-des-papes.com/fr" target="_blank"><img src="img/lieux-historiques/Palais_Des_Papes.jpg"></a>
-					<a href="http://www.marseilletourisme.fr/les-eglises/cathedrale-major/" target="_blank"><img src="img/lieux-historiques/Cathédrale_De_La_Major.jpg"></a>
-					<a href="http://www.notredamedelagarde.com/" target="_blank"><img src="img/lieux-historiques/Notre_Dame_De_La_Garde.jpg"></a>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Palais_Des_Papes.jpg" class="img-fluid">
+						<a href="http://www.palais-des-papes.com/fr" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Palais Des Papes</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Cathédrale_De_La_Major.jpg" class="img-fluid">
+						<a href="http://www.marseilletourisme.fr/les-eglises/cathedrale-major/" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Cathédrale De La Major</p>
+								</div>
+							</div>
+						</a>
+					</div>
+				</div>
+				<div class="col-xs-12 col-sm-4">
+					<div class="view overlay hm-black-strong">
+						<img src="img/lieux-historiques/Notre_Dame_De_La_Garde.jpg" class="img-fluid">
+						<a href="http://www.notredamedelagarde.com/" target="_blank">
+							<div class="mask text-center">
+								<div>
+									<p>Notre Dame De La Garde</p>
+								</div>
+							</div>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
